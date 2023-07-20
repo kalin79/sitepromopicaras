946a632213b4ceb06822e74c45ef354c28593bbc
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2023-07-20 16:00:21 -0500

    listo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
+import Script from 'next/script'
 import 'bootstrap/dist/css/bootstrap.css'
 import '@/styles/sass/globals.sass'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    let GTM_ID = process.env.NEXT_PUBLIC_GTM
+    console.log(GTM_ID)
+    return (
+        <>
+            <Script 
+                strategy="afterInteractive"  
+                src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+            />
+            <Script id="google-analytics" strategy="afterInteractive"  >
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GTM_ID}');
+                `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    )
 }
