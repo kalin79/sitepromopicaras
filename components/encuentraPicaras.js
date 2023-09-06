4b7b40c0ee4e1f5d15f84cdb1b0da40a92e3673b
Mery Andia <mery.andia.18@gmail.com>
2023-09-06 15:22:34 -0500

    encuentraTusPicaras

--- components/encuentraPicaras.js
+++ components/encuentraPicaras.js
@@ -34,17 +34,37 @@ const EncuentraPicaras = () => {
             <div className='' id='encuentraPicaras'>
                 <div className='container'>
 
-                    <div className={styles.boxFind} style={fontMonserratRegular.style}>
-                        <div>
+                    <div className={styles.boxFind}>
 
-                            <div className={styles.boxTitle}>
-                                <h2>
-                                    <Image src='/assets/title2.svg' width="430" height="69" alt='Titulo' />
-                                    <Image src='/assets/plane1.png' className={styles.avion1} width="452" height="242" alt='Avion' />
-                                    <Image src='/assets/plane2.png' className={styles.avion2} width="339" height="278" alt='Avion' />
-                                </h2>
+                        <div className={styles.boxTitle}>
+                            <h2>
+                                <Image src='/assets/title3.svg' width="430" height="69" alt='Titulo' />
+                                <Image src='/assets/plane1.png' className={styles.avion1} width="452" height="242" alt='Avion' />
+                                <Image src='/assets/plane2.png' className={styles.avion2} width="339" height="278" alt='Avion' />
+                            </h2>
+                            <h3 style={fontMonserratBold.style}>
+                                <Image src='/assets/franjaYellow.png' className={styles.franjaYellow} width="561" height="65" alt='FranjaAmarilla' />
+                                <p>En tiendas y supermercados a nivel nacional.</p>
+                            </h3>
+                        </div>
 
+                        <div className={styles.boxSquares}>
+                            <div className={styles.boxSquare}>
+                                <Image src='/assets/metro.svg' width="155" height="154" alt='Logo' />
                             </div>
+                            <div className={styles.boxSquare}>
+                                <Image src='/assets/plazavea.svg' width="154" height="54" alt='Logo' />
+                            </div>
+                            <div className={styles.boxSquare}>
+                                <Image src='/assets/wong.svg' width="155" height="156" alt='Logo' />
+                            </div>
+                            <div className={styles.boxSquare}>
+                                <Image src='/assets/rappi.svg' width="200" height="200" alt='Logo' />
+                            </div>
+                            <div className={styles.boxSquare}>
+                                <Image src='/assets/pedidosya.svg' width="171" height="106" alt='Logo' />
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
